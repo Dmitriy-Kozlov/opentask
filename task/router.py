6c9f4fae0069bf47c0532177db308e3844803d1e
Dimitrii-Kozlov <demetra_n@front.ru>
2024-07-24 23:13:45 +0300

    add filter to /me router

--- task/router.py
+++ task/router.py
@@ -1,15 +1,12 @@
-from typing import List
-
 from fastapi import APIRouter, Depends
-from sqlalchemy import select, and_
-from sqlalchemy.orm import joinedload, selectinload
+from sqlalchemy import select
+from sqlalchemy.orm import joinedload
 
-from auth.schemas import UserRel
 from auth.usermanager import current_active_user
 from database import get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
 from .models import Task, UserTask
-from .schemas import TaskRel
+from .schemas import TaskRel, TaskUserRead
 from auth.models import User
 
 router = APIRouter(
@@ -26,15 +23,20 @@ async def get_tasks(session: AsyncSession = Depends(get_async_session)):
     return tasks
 
 
-@router.get("/me", response_model=UserRel)
+@router.get("/me", response_model=list[TaskUserRead])
 async def get_tasks(session: AsyncSession = Depends(get_async_session),
-                    user: User = Depends(current_active_user)):
+                    user: User = Depends(current_active_user),
+                    is_completed: bool = None
+                    ):
     query = (
-        select(User)
-        .options(joinedload(User.tasks).joinedload(UserTask.task))
-        .filter_by(id=user.id)
+        select(UserTask)
+        .options(joinedload(UserTask.task))
+        .filter(UserTask.users_id == user.id)
     )
+    if is_completed is not None:
+        query = query.filter(UserTask.completed == is_completed)
+
     result = await session.execute(query)
-    tasks = result.unique().scalars().first()
+    tasks = result.unique().scalars().all()
 
     return tasks
